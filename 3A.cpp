1613c61c1a52e577bdf9c418365836f44c848feb
destinyson <1551430691@qq.com>
2020-06-26 10:47:44 +0800

    3A-Shortest path of the king

---
+++ 3A.cpp
@@ -0,0 +1,85 @@
+﻿/*******************************3A. Shortest path of the king*******************************
+time limit per test:1 second
+memory limit per test:64 megabytes
+input:standard input
+output:standard output
+
+The king is left alone on the chessboard. In spite of this loneliness, he doesn't lose heart, because he has business of national importance. For example, he has to pay an official visit to square t. As the king is not in habit of wasting his time, he wants to get from his current position s to square t in the least number of moves. Help him to do this.
+
+In one move the king can get to the square that has a common side or a common vertex with the square the king is currently in (generally there are 8 different squares he can move to).
+
+Input
+The first line contains the chessboard coordinates of square s, the second line — of square t.
+
+Chessboard coordinates consist of two characters, the first one is a lowercase Latin letter (from a to h), the second one is a digit from 1 to 8.
+
+Output
+In the first line print n — minimum number of the king's moves. Then in n lines print the moves themselves. Each move is described with one of the 8: L, R, U, D, LU, LD, RU or RD.
+
+L, R, U, D stand respectively for moves left, right, up and down (according to the picture), and 2-letter combinations stand for diagonal moves. If the answer is not unique, print any of them.
+
+Examples
+input
+a8
+h1
+output
+7
+RD
+RD
+RD
+RD
+RD
+RD
+RD
+
+********************************************************************************************/
+
+// 由于斜着走是横纵坐标均移动一格
+
+// 因此棋子的横纵坐标移动格数中的较大值即为最少移动步数
+
+// 贪心算法，先斜着走，然后直走弥补坐标差值
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string start, finish;
+	cin >> start >> finish;
+	int start_x = (int)start[0] - 97;
+	int start_y = (int)start[1] - 48;
+	int finish_x = (int)finish[0] - 97;
+	int finish_y = (int)finish[1] - 48;
+	int move_x = finish_x - start_x;
+	int move_y = finish_y - start_y;
+	cout << max(abs(move_x), abs(move_y)) << endl;
+	while (move_x || move_y)
+	{
+		if (move_x < 0)
+		{
+			cout << "L";
+			move_x++;
+		}
+		else if (move_x > 0)
+		{
+			cout << "R";
+			move_x--;
+		}
+		if (move_y < 0)
+		{
+			cout << "D";
+			move_y++;
+		}
+		else if (move_y > 0)
+		{
+			cout << "U";
+			move_y--;
+		}
+		cout << endl;
+	}
+	return 0;
+}
