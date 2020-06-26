64c757b6c6aacf990c80f6a517ad216c4fbccb61
destinyson <1551430691@qq.com>
2020-06-26 11:02:31 +0800

    3C-Tic-tac-toe

---
+++ 3C.cpp
@@ -0,0 +1,111 @@
+﻿/**************************************3C. Tic-tac-toe**************************************
+time limit per test:1 second
+memory limit per test:64 megabytes
+input:standard input
+output:standard output
+
+Certainly, everyone is familiar with tic-tac-toe game. The rules are very simple indeed. Two players take turns marking the cells in a 3 × 3 grid (one player always draws crosses, the other — noughts). The player who succeeds first in placing three of his marks in a horizontal, vertical or diagonal line wins, and the game is finished. The player who draws crosses goes first. If the grid is filled, but neither Xs, nor 0s form the required line, a draw is announced.
+
+You are given a 3 × 3 grid, each grid cell is empty, or occupied by a cross or a nought. You have to find the player (first or second), whose turn is next, or print one of the verdicts below:
+
+illegal — if the given board layout can't appear during a valid game;
+the first player won — if in the given board layout the first player has just won;
+the second player won — if in the given board layout the second player has just won;
+draw — if the given board layout has just let to a draw.
+
+Input
+The input consists of three lines, each of the lines contains characters ".", "X" or "0" (a period, a capital letter X, or a digit zero).
+
+Output
+Print one of the six verdicts: first, second, illegal, the first player won, the second player won or draw.
+
+Examples
+input
+X0X
+.0.
+.X.
+output
+second
+********************************************************************************************/
+
+// 要搞清楚哪些情况是合法的硬性要求：
+
+// 先手步数不能小于后手，而且至多比后手多一步
+
+// 双方不能都赢
+
+// 双方都没赢的情况下，不能留白
+
+// 先手赢时，双方步数不能相等
+
+// 后手赢时，双方步数必须相等
+
+// 符合这些规则，才能判定胜负，否则就是不合法的
+
+#include <iostream>
+
+using namespace std;
+
+char m[3][3];
+
+bool count_line(char c) {
+    if (m[0][0] == c && m[0][1] == c && m[0][2] == c)
+        return true;
+    if (m[1][0] == c && m[1][1] == c && m[1][2] == c)
+        return true;
+    if (m[2][0] == c && m[2][1] == c && m[2][2] == c)
+        return true;
+    if (m[0][0] == c && m[1][0] == c && m[2][0] == c)
+        return true;
+    if (m[0][1] == c && m[1][1] == c && m[2][1] == c)
+        return true;
+    if (m[0][2] == c && m[1][2] == c && m[2][2] == c)
+        return true;
+    if (m[0][0] == c && m[1][1] == c && m[2][2] == c)
+        return true;
+    if (m[0][2] == c && m[1][1] == c && m[2][0] == c)
+        return true;
+    return false;
+}
+
+int main()
+{
+    while (cin >> m[0][0] >> m[0][1] >> m[0][2] >> m[1][0] >> m[1][1] >> m[1][2] >> m[2][0] >> m[2][1] >> m[2][2]) {
+        int cross = 0, round = 0, blank = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (m[i][j] == 'X')
+                    cross++;
+                else if (m[i][j] == '0')
+                    round++;
+                else
+                    blank++;
+            }
+        }
+        bool first = count_line('X');
+        bool second = count_line('0');
+        if (cross < round || cross - round > 1)
+            cout << "illegal" << endl;
+        else if (first && second)
+            cout << "illegal" << endl;
+        else if (first) {
+            if (cross == round)
+                cout << "illegal" << endl;
+            else
+                cout << "the first player won" << endl;
+        }
+        else if (second) {
+            if (cross - round == 1)
+                cout << "illegal" << endl;
+            else
+                cout << "the second player won" << endl;
+        }
+        else if (!blank)
+            cout << "draw" << endl;
+        else if (cross == round)
+            cout << "first" << endl;
+        else
+            cout << "second" << endl;
+    }
+    return 0;
+}
